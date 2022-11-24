915dbc16efeebad8bf8d8a3141c53e3b609406ed
Andrew Koo <andrewkoo@Andrews-MBP-16.lan>
2022-11-24 14:47:44 -0500

    include loading message while connecting to database

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Goal from "../components/Goal";
 import { useGoalsContext } from "../hooks/useGoalsContext";
-import { Box } from "@chakra-ui/react";
+import { Box, Spinner, Kbd, Center, Alert, AlertIcon } from "@chakra-ui/react";
 
 const Home = () => {
   const { goals, dispatch } = useGoalsContext();
@@ -21,7 +21,31 @@ const Home = () => {
   }, [dispatch]);
   return (
     <Box>
-      {goals && goals.map((goal) => <Goal goal={goal} key={goal._id} />)}
+      {goals ? (
+        goals.map((goal) => <Goal goal={goal} key={goal._id} />)
+      ) : (
+        <>
+          <Box>
+            <Alert status="warning">
+              <AlertIcon />
+              Connecting to database.. may need to refresh several times
+            </Alert>
+            <Box mt={5}>
+              <Center>
+                <span>
+                  Press <Kbd>F5</Kbd> or <Kbd>command</Kbd>/<Kbd>control</Kbd> +{" "}
+                  <Kbd>r</Kbd> to refresh the page
+                </span>
+              </Center>
+            </Box>
+          </Box>
+          <Box mt={5}>
+            <Center>
+              <Spinner />
+            </Center>
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
